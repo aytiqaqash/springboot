4c4fa60d3eeb6495ec214452859cef8a63b1a91c
aytiqaqash <aytiqaqash@gmail.com>
2022-12-17 22:30:40 -0500

    Add Student Entity
    Description: Student Entity is the class with annotations to create students table in DB and create necessary columns

---
+++ src/main/java/com/example/springboot/entity/Student.java
@@ -0,0 +1,31 @@
+package com.example.springboot.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "students")
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="first_name", nullable = false)
+    private String firstName;
+
+    @Column(name="last_name", nullable = false, columnDefinition = "LONG TEXT")
+    private String lastName;
+
+    @Column(name="date_of_birth", nullable = false)
+    private Integer dateOfBirth;
+
+    @Column(nullable = false, unique = true)
+    private Integer number;
+
+
+}
