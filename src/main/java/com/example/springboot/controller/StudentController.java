65127eb80dcf34f94650ed2307f92edf5eac1d69
aytiqaqash <aytiqaqash@gmail.com>
2022-12-17 22:35:00 -0500

    Add Student Controller and Service
    Description: Created Student Controller and Service with ioC and DI

---
+++ src/main/java/com/example/springboot/controller/StudentController.java
@@ -0,0 +1,16 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.service.StudentService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+}
---
+++ src/main/java/com/example/springboot/service/StudentService.java
@@ -0,0 +1,15 @@
+package com.example.springboot.service;
+
+
+import com.example.springboot.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+}
