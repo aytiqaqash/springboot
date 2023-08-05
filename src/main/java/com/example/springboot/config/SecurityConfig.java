9b1f8fe08fd1e3e21084459f7638fa99e423336d
aytiqaqash <aytiqaqash@gmail.com>
2023-08-05 10:26:26 -0400

    Custom user auth
    Description:
    Created PasswordConfig class to encrypt the password. Added User details in SecurityConfig class.

--- src/main/java/com/example/springboot/config/SecurityConfig.java
+++ src/main/java/com/example/springboot/config/SecurityConfig.java
@@ -1,16 +1,37 @@
 package com.example.springboot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    private final  PasswordConfig passwordConfig;
+//
+//    public SecurityConfig(PasswordConfig passwordConfig) {
+//        this.passwordConfig = passwordConfig;
+//    }
+
+    private  PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/","index", "/css/*", "/js/*")
                 .permitAll()
@@ -18,5 +39,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService(){
+        UserDetails ziya = User.builder()
+                .username("ziya")
+                .password(passwordEncoder.encode("password"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails aytiqaqash = User.builder()
+                .username("aytiqaqash")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(ziya,aytiqaqash);
     }
 }
